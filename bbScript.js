3e9f5fa3781a86f34d2e0a5e12f19df438c31437
James Middlemiss <middlemissj.usa@gmail.com>
2016-03-16 16:27:19 -0400

    Adds CORS functionality to script   - Allows and instantiates CORS   - Fixes initial current visitor request   - Adds listener when current visitor has no email

--- bbScript.js
+++ bbScript.js
@@ -1,12 +1,32 @@
-salemoveApi.addEventListener(
-  salemoveApi.EVENTS.ENGAGEMENT_START,
-    function(engagement){
-      console.log("the engagement with id " + engagement.engagementId + " has begun!")
-    }
+var cbAfterSaleMoveIsReady = function(salemoveApi){
+  salemoveApi.addEventListener(
+    salemoveApi.EVENTS.ENGAGEMENT_START,
+      function(engagement){
+        console.log("the engagement with id " + engagement.engagementId + " has begun!")
+      }
+  );
+  salemoveApi.addEventListener(
+    salemoveApi.EVENTS.ENGAGEMENT_START,
+      currentVisitorRequest
+  );
+  salemoveApi.addEventListener(
+    salemoveApi.EVENTS.ENGAGEMENT_END,
+      function(engagement){ 
+        console.log('the engagement with id ' + engagement.engagementId + ' ended!');
+      }
+  );
+}
+
+sm.getApi({version:'v1'}).then(
+  function(api){
+    window.salemoveApi = api;
+    cbAfterSaleMoveIsReady(api);
+    console.log("got the API" + api)
+  },
+  function(err){ 
+    console.log("An error occured: ", err
+  )}
 );
-salemoveApi.addEventListener(
-  salemoveApi.EVENTS.ENGAGEMENT_START,
-  checkEmail);
 
 function checkEmail(){
   var email = $('.sm-user-email').val()
@@ -16,84 +36,117 @@ function checkEmail(){
   }
 }
 
-salemoveApi.addEventListener(
-  salemoveApi.EVENTS.ENGAGEMENT_END,
-    function(engagement){ 
-      console.log('the engagement with id ' + engagement.engagementId + ' ended!');
-    }
-);
-
-
 function addListener(){
+    alert('Listener added')
   $(document.body).on('change','.sm-user-email',function(){
     alert('Change Happened, look for new info.');
     postToRubyApp();
-});
+  });
 }
 
 function postToRubyApp(){
-  var email = $('.sm-user-email').val()
-  console.log(email + " this is inside rubyfunction")
+  var email = $('.sm-user-email').val();
+  var accept = 'application/vnd.salemove.v1+json'
+  var body =  {
+                'email': email,
+                "Authorization": requestHeadersAuth,
+                "Accept": accept,
+                "X-Salemove-Visit-Session-Id": requestHeadersSession
+              }
+
   $.ajax({
-    type: "POST",
-    data: JSON.stringify(email),
-    url: "http://localhost:3000/",
+    type: "GET",
+    data: body,
+    url: "http://localhost:3000/tickets",
     contentType: "application/json"
   })
-  console.log("Ajax Request success?")
+      console.log("Ajax Request success?")
 }
 
-sm.getApi({version:'v1'}).then(
+function currentVisitorRequest(api){
 
-  function(api){
-    console.log("I got the api!", api);
-    window.salemoveApi = api;
-  },
-  function(err){ console.log("An error occured: ", err)}
-);
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.salemove.com/visitor');
 
+  window.requestHeadersAuth = salemoveApi.getRequestHeaders()['Authorization']
+  window.requestHeadersSession = salemoveApi.getRequestHeaders()['X-Salemove-Visit-Session-Id']
 
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  if ("withCredentials" in xhr) {
+  request.setRequestHeader('Authorization',
+                            requestHeadersAuth)
+  request.setRequestHeader('Accept',
+                            'application/vnd.salemove.v1+json');
+  request.setRequestHeader('X-Salemove-Visit-Session-Id',
+                            requestHeadersSession)
 
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
+  request.onreadystatechange = function(){
+    if (this.readyState === 4){
+      var body = this.responseText
+      console.log('Status:', this.status);
+      console.log('Headers:', this.getAllResponseHeaders() );
+      console.log('Body:', this.responseText);
+      console.log(this);
 
-  } else if (typeof XDomainRequest != "undefined") {
+      window.parsed = JSON.parse(body);
 
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
+      if(parsed.email === null || "" || undefined){
+        addListener();
+      } else {
+        // requestZenDesk(parsed.email)
+        console.log("we got an email, yo")
+      }
+    }
+  };
+  request.send();
 }
 
-var xhr = createCORSRequest('GET', "https://tranquil-temple-7492.herokuapp.com");
-if (!xhr) {
-  throw new Error('CORS not supported');
-}
+// function createCORSRequest(method, url) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.withCredentials = true;
+//   if ("withCredentials" in xhr) {
 
-xhr.onload = function() {
- var responseText = xhr.responseText;
+//     // Check if the XMLHttpRequest object has a "withCredentials" property.
+//     // "withCredentials" only exists on XMLHTTPRequest2 objects.
+//     xhr.open(method, url, true);
 
- console.log(responseText);
- // process the response.
-};
+//   } else if (typeof XDomainRequest != "undefined") {
 
-xhr.onerror = function() {
-  console.log(xhr.err)
-  console.log('There was an error!');
-};
+//     // Otherwise, check if XDomainRequest.
+//     // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+//     xhr = new XDomainRequest();
+//     xhr.open(method, url);
+
+//   } else {
+
+//     // Otherwise, CORS is not supported by the browser.
+//     xhr = null;
+
+//   }
+//   return xhr;
+// }
+
+// var xhr = createCORSRequest('GET', "http://localhost:3000/tickets");
+// if (!xhr) {
+//   throw new Error('CORS not supported');
+// }
+// xhr.onloadstart = function() {
+//   data = {
+//     payload: "hello world",
+//     contentType: "application/json"
+//   }
+// }
+// xhr.onload = function() {
+//  var responseText = xhr.responseText;
+
+//  console.log(responseText);
+//  // process the response.
+// };
+
+// xhr.onerror = function() {
+//   console.log(xhr.err)
+//   console.log('There was an error!');
+// };
 
 
-xhr.send()
+// xhr.send()
+
+
