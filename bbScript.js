5dc61a25c4c57561560f875d18825d4c9957d745
James Middlemiss <middlemissj.usa@gmail.com>
2016-03-16 17:08:59 -0400

    Adds filters to script and cleans dead code.   - removes comments and console.logs   - filters "" email   - adds email to inital request when visitor already has email

--- bbScript.js
+++ bbScript.js
@@ -21,46 +21,36 @@ sm.getApi({version:'v1'}).then(
   function(api){
     window.salemoveApi = api;
     cbAfterSaleMoveIsReady(api);
-    console.log("got the API" + api)
   },
   function(err){ 
     console.log("An error occured: ", err
   )}
 );
 
-function checkEmail(){
-  var email = $('.sm-user-email').val()
-  console.log(email)
-  if (email ===  undefined || "" || null) {
-    addListener();
-  }
-}
-
 function addListener(){
-    alert('Listener added')
   $(document.body).on('change','.sm-user-email',function(){
-    alert('Change Happened, look for new info.');
-    postToRubyApp();
+    window.email = $('.sm-user-email').val();
+    sendToRubyApp();
   });
 }
 
-function postToRubyApp(){
-  var email = $('.sm-user-email').val();
-  var accept = 'application/vnd.salemove.v1+json'
-  var body =  {
-                'email': email,
-                "Authorization": requestHeadersAuth,
-                "Accept": accept,
-                "X-Salemove-Visit-Session-Id": requestHeadersSession
-              }
+function sendToRubyApp(){
+  if (email !== "" || null || undefined){
+    var accept = 'application/vnd.salemove.v1+json'
+    var body =  {
+                  'email': email,
+                  "Authorization": requestHeadersAuth,
+                  "Accept": accept,
+                  "X-Salemove-Visit-Session-Id": requestHeadersSession
+                }
 
-  $.ajax({
-    type: "GET",
-    data: body,
-    url: "http://localhost:3000/tickets",
-    contentType: "application/json"
-  })
-      console.log("Ajax Request success?")
+    $.ajax({
+      type: "GET",
+      data: body,
+      url: "http://localhost:3000/tickets",
+      contentType: "application/json"
+    })
+  }
 }
 
 function currentVisitorRequest(api){
@@ -80,73 +70,16 @@ function currentVisitorRequest(api){
 
   request.onreadystatechange = function(){
     if (this.readyState === 4){
-      var body = this.responseText
-      console.log('Status:', this.status);
-      console.log('Headers:', this.getAllResponseHeaders() );
-      console.log('Body:', this.responseText);
-      console.log(this);
 
-      window.parsed = JSON.parse(body);
+      var parsedResponse = JSON.parse(this.responseText);
 
-      if(parsed.email === null || "" || undefined){
+      if(parsedResponse.email === null || "" || undefined){
         addListener();
       } else {
-        // requestZenDesk(parsed.email)
-        console.log("we got an email, yo")
+        window.email = parsedResponse.email
+        sendToRubyApp()
       }
     }
   };
   request.send();
 }
-
-// function createCORSRequest(method, url) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.withCredentials = true;
-//   if ("withCredentials" in xhr) {
-
-//     // Check if the XMLHttpRequest object has a "withCredentials" property.
-//     // "withCredentials" only exists on XMLHTTPRequest2 objects.
-//     xhr.open(method, url, true);
-
-//   } else if (typeof XDomainRequest != "undefined") {
-
-//     // Otherwise, check if XDomainRequest.
-//     // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-//     xhr = new XDomainRequest();
-//     xhr.open(method, url);
-
-//   } else {
-
-//     // Otherwise, CORS is not supported by the browser.
-//     xhr = null;
-
-//   }
-//   return xhr;
-// }
-
-// var xhr = createCORSRequest('GET', "http://localhost:3000/tickets");
-// if (!xhr) {
-//   throw new Error('CORS not supported');
-// }
-// xhr.onloadstart = function() {
-//   data = {
-//     payload: "hello world",
-//     contentType: "application/json"
-//   }
-// }
-// xhr.onload = function() {
-//  var responseText = xhr.responseText;
-
-//  console.log(responseText);
-//  // process the response.
-// };
-
-// xhr.onerror = function() {
-//   console.log(xhr.err)
-//   console.log('There was an error!');
-// };
-
-
-// xhr.send()
-
-
